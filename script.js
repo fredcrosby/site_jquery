9c5935df0a157e22ac9374380d8537f1a99ce8e3
Frédéric Roy <frederic.roy.concepteur@gmail.com>
2024-04-14 15:09:02 -0400

    essai 2anime

--- script.js
+++ script.js
@@ -3,4 +3,19 @@ $(function () {
   $("#p1").click(function () {
     $("#p5").hide();
   });
+
+  $("#start").click(function () {
+    $(".animate").animate(
+      {
+        left: "600px",
+        opacity: "0.2",
+        with: "+=150px",
+        height: "+=150px",
+      },
+      5000
+    );
+  });
+  $("#stop").click(function () {
+    $(".animate").stop();
+  });
 });
